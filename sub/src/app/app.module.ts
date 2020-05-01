d438d70bcb7234aec42382d4c81c50aa868e0265
sanura-njaka <40278046+sanura-njaka@users.noreply.github.com>
2020-04-30 20:57:03 -0500

    add src/app files

---
+++ sub/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig, 'sub-help')
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
