daca25361abab3e79037e1dae7c573135733c7d5
duniahakim <doniahakim@gmail.com>
2020-05-05 00:04:37 -0700

    linked home page to MessagingComponent and added node_modules to gitignore

--- madison-trial/src/app/app-routing.module.ts
+++ madison-trial/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/home.component'; 
-import { NewUserComponent } from './new-user/new-user.component'; 
+import { HomeComponent } from './home/home.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { MessagingComponent } from './messaging/messaging.component';
 
 const routes: Routes = [
 	{ path: '', component: HomeComponent },
-	{ path: 'new-user', component: NewUserComponent } 
+	{ path: 'new-user', component: NewUserComponent },
+	{ path: 'messaging', component: MessagingComponent }
 ];
 
 @NgModule({
