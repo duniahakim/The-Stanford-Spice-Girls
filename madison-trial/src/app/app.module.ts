f52563ef1039d4f5e957d3009ff8e92c8d3b1537
duniahakim <doniahakim@gmail.com>
2020-05-11 12:16:29 -0700

    added angularfirebase objects to save messages

--- madison-trial/src/app/app.module.ts
+++ madison-trial/src/app/app.module.ts
@@ -13,6 +13,18 @@ import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { MessagingComponent } from './messaging/messaging.component';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAscTztvYV091FR8nUcX9cQBf5SIGHghaM",
+  authDomain: "cs194-48dd8.firebaseapp.com",
+  databaseURL: "https://cs194-48dd8.firebaseio.com",
+  projectId: "cs194-48dd8",
+  storageBucket: "cs194-48dd8.appspot.com",
+  messagingSenderId: "124531208729",
+  appId: "1:124531208729:web:52cb027f8f490267cab088",
+  measurementId: "G-HP69NBHD3Y"
+};
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +37,8 @@ import { MessagingComponent } from './messaging/messaging.component';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
  	  AngularFirestoreModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- madison-trial/src/app/messaging/messaging.component.ts
+++ madison-trial/src/app/messaging/messaging.component.ts
@@ -1,10 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2'; // deleted AuthProviders, AuthMethods
 
 @Component({
   selector: 'app-messaging',
   templateUrl: './messaging.component.html',
   styleUrls: ['./messaging.component.css']
 })
+
+export class AppComponent {
+  items: FirebaseListObservable<any>;
+  name: any;
+  msgVal: string = '';
+
+  constructor(public af: AngularFire) {
+      this.items = af.database.list('/messages', {
+        query: {
+          limitToLast: 5
+        }
+      });
+  }
+}
+
 export class MessagingComponent implements OnInit {
 
   constructor() { }
