e43d847ce64b4eb6eaf34c6203c89696718b94e8
duniahakim <doniahakim@gmail.com>
2020-05-28 19:17:57 -0700

    fixed head about style changing for nav bar in messaging

--- madison-trial/src/app/messaging/messaging.component.ts
+++ madison-trial/src/app/messaging/messaging.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from "@angular/fire/database";  // deleted AuthProviders, AuthMethods
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -7,6 +7,7 @@ import { User } from 'firebase';
 
 @Component({
   selector: 'app-messaging',
+  encapsulation: ViewEncapsulation.ShadowDom,
   templateUrl: './messaging.component.html',
   styleUrls: ['./messaging.component.css']
 })
